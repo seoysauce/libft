822f7e21e5503224cb153310b9548a7f62d89ee5
Seoyoung Jeong <issyo1022@gmail.com>
2020-08-18 14:20:18 +0900

    FIN

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: seojeong <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/12 17:18:37 by seojeong          #+#    #+#             */
-/*   Updated: 2020/07/30 19:20:22 by seojeong         ###   ########.fr       */
+/*   Updated: 2020/08/17 13:44:07 by seojeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ char	*ft_strdup(const char *s1)
 	char	*duplicate;
 
 	len = ft_strlen(s1);
-	if (!(duplicate = ft_calloc(len + 1, sizeof(char))))
-		return (NULL);
 	duplicate = ft_calloc(len + 1, sizeof(char));
+	if (!duplicate)
+		return (NULL);
 	ft_memcpy(duplicate, s1, len);
 	duplicate[len] = '\0';
 	return (duplicate);
